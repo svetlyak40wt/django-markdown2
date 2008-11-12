a25ed6121b5764888dffab74bdf86a50ad3aa505
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-12 03:33:29 +0300

    Version 0.1.2, without explicit dependency from the Django.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-markdown2',
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'This is a simple app, which supplies a single template tag for markdown markup.',
     keywords = 'django apps utils',
     license = 'New BSD License',
     author = 'Alexander Artemenko',
     author_email = 'svetlyak.40wt@gmail.com',
     url = 'http://github.com/svetlyak40wt/django-markdown2/',
-    install_requires = ['markdown2==1.0.0.11', 'Django>=1.0'],
-    dependency_links = ['http://pypi.aartemenko.com', 'http://aartemenko.com/media/packages.html'],
+    install_requires = ['markdown2==1.0.0.11'],
+    #dependency_links = ['http://pypi.aartemenko.com', 'http://aartemenko.com/media/packages.html'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
